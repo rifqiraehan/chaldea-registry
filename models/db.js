af53a071e0191fef1cf362329372323a62806f9a
rifqiraehan <rifqiraehan86@gmail.com>
2025-07-14 19:18:27 +0700

    refactor database connection logic to use a connection status flag

--- models/db.js
+++ models/db.js
@@ -3,10 +3,12 @@ require('dotenv').config({ quiet: true });
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const dbName = process.env.DATABASE_NAME || 'fgo';
+let isConnected = false;
 
 async function connectDB() {
-  if (!client.isConnected) {
+  if (!isConnected) {
     await client.connect();
+    isConnected = true;
   }
   return client.db(dbName);
 }
