1ebe4f21c949f2007db520af3f935dc13d420e1d
rifqiraehan <rifqiraehan86@gmail.com>
2025-07-16 10:42:53 +0700

    add CORS headers to JSON response utility

--- utils/sendJson.js
+++ utils/sendJson.js
@@ -1,4 +1,9 @@
 module.exports = function sendJson(res, statusCode, data) {
-  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.writeHead(statusCode, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+  });
   res.end(JSON.stringify(data));
 };
